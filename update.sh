7f243b36ade73675f319a95591d7638895d4aa71
Lisette <pyy20041231@outlook.com>
2025-08-26 21:57:53 +0800

    :pencil: update content

--- update.sh
+++ update.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
-# 提交信息
 info=$1
 if [ "$info" == "" ]; then
     info=":pencil: update content"
 fi
 
 # 生成静态文件（生产模式，走 CDN）
-cross-env NODE_ENV=production hexo g
+npx cross-env NODE_ENV=production hexo g
 
 # 部署到 GitHub Pages
-cross-env NODE_ENV=production hexo d
+npx cross-env NODE_ENV=production hexo d
 
 # 提交源码改动到 hexo 分支
 git add -A
