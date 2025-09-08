237a36be32a15243b8cb6748bfd080bf4c7cab04
Lisette <pyy20041231@outlook.com>
2025-09-08 21:42:40 +0800

    migrate to valaxy

---
+++ site.config.ts
@@ -0,0 +1,102 @@
+import { defineSiteConfig } from 'valaxy'
+
+export default defineSiteConfig({
+  url: 'https://valaxy.site/',
+  lang: 'zh-CN',
+  title: '01paw',
+  author: {
+    name: 'Lisette',
+    avatar: 'https://www.yunyoujun.cn/images/avatar.jpg',
+  },
+  /**
+   * 站点图标
+   */
+  favicon: 'https://www.yunyoujun.cn/favicon.svg',
+  /**
+   * 副标题
+   */
+  subtitle: 'check out my blog',
+  description: '有很多话想说......',
+  social: [
+    {
+      name: 'GitHub',
+      link: 'https://github.com/1231Lisette',
+      icon: 'i-ri-github-line',
+      color: '#6e5494',
+    },
+    {
+      name: '微博',
+      link: 'https://weibo.com/巷尾的贻猫',
+      icon: 'i-ri-weibo-line',
+      color: '#E6162D',
+    },
+    {
+      name: 'E-Mail',
+      link: 'mailto:me@2405848522qq.com',
+      icon: 'i-ri-mail-line',
+      color: '#8E71C1',
+    },
+  ],
+
+  search: {
+    enable: false,
+  },
+
+  sponsor: {
+    enable: true,
+    title: '我很可爱，请给我钱！',
+    methods: [
+      {
+        name: '支付宝',
+        url: 'https://cdn.yunyoujun.cn/img/donate/alipay-qrcode.jpg',
+        color: '#00A3EE',
+        icon: 'i-ri-alipay-line',
+      },
+      {
+        name: 'QQ 支付',
+        url: 'https://cdn.yunyoujun.cn/img/donate/qqpay-qrcode.png',
+        color: '#12B7F5',
+        icon: 'i-ri-qq-line',
+      },
+      {
+        name: '微信支付',
+        url: 'https://cdn.yunyoujun.cn/img/donate/wechatpay-qrcode.jpg',
+        color: '#2DC100',
+        icon: 'i-ri-wechat-pay-line',
+      },
+    ],
+  },
+    /**
+   * 开启阅读统计
+   */
+  statistics: {
+    enable: true,
+    readTime: {
+      /**
+       * 阅读速度
+       */
+      speed: {
+        cn: 300,
+        en: 200,
+      },
+    },
+  },
+  //代码高亮
+  codeHeightLimit: 300,
+  ///foo, /bar, /v1/about 这些路由会被重定向到 /about。
+  redirects: {
+    useVueRouter: true,
+    rules: [
+      {
+        from: ['/foo', '/bar'],
+        to: '/about',
+      },
+      {
+        from: '/v1/about',
+        to: '/about',
+      },
+    ]
+  },
+  mediumZoom: { enable: true },
+  
+})
---
+++ valaxy.config.ts
@@ -0,0 +1,51 @@
+//siteConfig: 站点信息配置，这部分内容面向站点展示，且在不同主题中也是通用的格式
+//mthemeConfig: 主题配置，这部分内容仅在特定主题生效
+//runtimeConfig: 运行时的配置（由 Valaxy 自动生成），用户无需配置
+import type { UserThemeConfig } from 'valaxy-theme-yun'
+import { defineValaxyConfig } from 'valaxy'
+
+// add icons what you will need
+const safelist = [
+  'i-ri-home-line',
+]
+
+/**
+ * User Config
+ */
+export default defineValaxyConfig<UserThemeConfig>({
+  // site config see site.config.ts
+
+  theme: 'yun',
+
+  themeConfig: {
+    banner: {
+      enable: true,
+      title: '云游君的小站',
+    },
+
+    pages: [
+      {
+        name: '我的小伙伴们',
+        url: '/links/',
+        icon: 'i-ri-genderless-line',
+        color: 'dodgerblue',
+      },
+      {
+        name: '喜欢的女孩子',
+        url: '/girls/',
+        icon: 'i-ri-women-line',
+        color: 'hotpink',
+      },
+    ],
+
+    footer: {
+      since: 2016,
+      beian: {
+        enable: true,
+        icp: '苏ICP备17038157号',
+      },
+    },
+  },
+
+  unocss: { safelist },
+})
